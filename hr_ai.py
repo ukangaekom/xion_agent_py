9448a32982ac904e5d83a9ebcb981c6ff156ef86
Coin-boy <ekomabasiuk@gmail.com>
2025-01-11 13:45:19 +0100

    hr_agentic

--- hr_ai.py
+++ hr_ai.py
@@ -1,39 +1,50 @@
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
+from knowledge_base import knowledge
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 
-# instructions = "You are an AI Agent for Xion Blockchain"
 
-instructions = """In Context, you are considered to be an advanced crypto wizard for a   blockchain called Xion Blockchain. You are to leverage your large language model and internet searching capabailities to do the following:
+instructions = f"""In Context, you are considered to be an advanced crypto customer and representative wizard for a blockchain called Xion Blockchain. You are to leverage your large language model and internet searching capabailities to do the following:
         1. Search for current ongoing events based on Xion Blockchain Ecosystem.
         2. Search all blogs and websites related to Xion Blockchain and get data from there.
         3. Get data for all tokens launched on Xion Blockchain.
         4. Get any reachable valid and upto data data about projects based on Xion blockchain.
         
-        Note: These are your sources of informations. While you generate answers, your answers must correlate to this sources. No Hallucination.
+        Note: These are your sources of informations. While you generate answers, your answers must correlate to this sources. No Hallucination. Also, make sure your answers are short, brief and summarized while highlighting what is needed to be known.
         
         Following this data sources and Information, I name you ```Xion Agent```. You are the wizard of Xion Universe Blockchain Universe. Use this instructions to engage users more than any paid expert can do.
         
-        ALL THE INFORMATIONS FOR XION CAN BE FOUND HERE ```https://docs.burnt.com/xion```"""
+        ALL THE INFORMATIONS FOR XION CAN BE FOUND HERE ```https://docs.burnt.com/xion```
+        THE TWITTER OR X ACCOUNT CAN BE FOUND HERE ```https://x.com/burnt_xion```
+        THE DISCORD COMMUNITY HANDLE CAN BE FOUND HERE ```https://discord.gg/burnt```
+        THE OFFICIAL WEBSITE FOR XION IS ```https://xion.burnt.com```
+        THE OFFICIAL WEBSITE FOR THRIVE PROTOCOL ```https://thriveprotocol.com```
+        THE OFFICIAL TWITTER OR X HANDLE FOR THRIVE PROTOCOL ```https://x.com/thriveprotocol```
+        
+        NOTE: ALWAYS BE POLITE IN YOUR RESPONDS WITH SOME HUMAN HUMOR AND POSITIVE COMMUNICATION FEEDBACKS.
+        """
 
 
 model = genai.GenerativeModel(
     model_name="gemini-1.5-flash",
-    system_instruction = instructions
+    system_instruction = instructions,
+    
 )
 
 # model.generate_content()
-chat_session = model.start_chat()
+chat_session = model
+
+chat_session.max_output_token = 100
 
 
 # response = model.generate_content("Hello sir! I want to apply for a job.")
 # print(response.text)  
 def aiagent(message):
-    response = chat_session.send_message(message)
+    response = chat_session.generate_content([knowledge,message])
     print(response.text)
     return response.text
  
--- knowledge_base.py
+++ knowledge_base.py
@@ -1,11 +1,14 @@
 import pdfplumber
 
 
-knowledge_base = ""
+knowledge = ""
 # Open the PDF file
 with pdfplumber.open("whitepaper.pdf") as pdf:
     for page in pdf.pages:
         # print(page.extract_text())
-        knowledge_base += page.extract_text()
+        knowledge += page.extract_text()
 
-print(knowledge_base)
+
+
+if __name__ == "__main__":
+    pass
